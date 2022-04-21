620610e881799ec7d7a93c2e4df8335fc669c879
Mohammad S Anwar <Mohammad.Anwar@yahoo.com>
2022-04-21 22:40:56 +0100

    Update folding-case.pl

--- v516/folding-case.pl
+++ v516/folding-case.pl
@@ -9,7 +9,6 @@ use v5.16;
 print "Regular case matched.\n" if lc('AEIOU') eq 'aeiou';
 
 # For Unicode characters match use fc() as in v5.16.
-
 my $UPPER = 'ÄËÏÖÜ';
 my $LOWER = 'äëïöü';
 print "Folding case matched.\n" if fc($UPPER) cmp fc($LOWER);
