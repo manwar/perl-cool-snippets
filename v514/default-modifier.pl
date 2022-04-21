716b220cbeb4d6b864eee00ad3e1f2f92c7990a8
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-04-21 18:24:07 +0100

    - Tidied up comment.

--- v514/default-modifier.pl
+++ v514/default-modifier.pl
@@ -6,7 +6,7 @@ use warnings;
 use v5.14;
 
 {
-    #  it would apply to every regex in the scope.
+    # It would apply to every regex in the scope.
     use re '/i';
     $_ = 'Perl';
     print "matched using default modifier.\n" if /perl/;
