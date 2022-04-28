3a92e1efc93cf5d742ead8dedda4a2b43df1bf59
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-04-28 03:20:27 +0100

    - Updated line ending.

--- v510/line-ending.pl
+++ v510/line-ending.pl
@@ -5,18 +5,18 @@ use warnings;
 
 use v5.10;
 
-$_ = "Line 1\nLine 2\r\nLine 3";
+$_ = "Line 1\nLine 2\r\nLine 3\n\rLine 4";
 
 # Traditionally: catch line ending
 
-s/\n|\r\n/\|/g && print;
+s/\n|\r\n|\n\r\|/\n/g && print;
 
-print "\n";
+print "\n------\n";
 
 # Perl v5.10 introduced \R matching any line ending.
 
-$_ = "Line 1\nLine 2\r\nLine 3";
+$_ = "Line 1\nLine 2\r\nLine 3\n\rLine 4";
 
-s/\R/\|/g && print;
+s/\R+/\n/g && print;
 
-print "\n";
+print "\n------\n";
