00c110e05cbaa9bb0af5e3b2085141863f2354bd
William Ricker <bill.n1vux@gmail.com>
2022-05-10 14:37:03 -0400

    use feature, no warnings

--- v534/try-catch.pl
+++ v534/try-catch.pl
@@ -7,10 +7,12 @@ use v5.34;
 my $e = eval { 1/0 };
 print "exception: $@\n" if $@;
 
+use feature 'try';
+no warnings "experimental::try";
+
 # Perl v5.34 introduced try/catch block.
 try {
-    1/0;
-} catch {
-    my $e = shift;
+    my $x = 1/0;
+} catch ($e) {
     print "try/catch exception: $e\n";
 }
