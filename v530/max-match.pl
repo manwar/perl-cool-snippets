f24689343faa883cafe13ca068b7ab4a0a7291cf
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-05-16 13:45:58 +0100

    - Tidied up max match.

--- v530/max-match.pl
+++ v530/max-match.pl
@@ -3,20 +3,17 @@
 use strict; use warnings;
 use v5.30;
 
-# I am pretty sure you have use regex quantifier {n,}.
+# I am pretty sure you have used regex quantifier {n,}.
 #
 # What does this mean?
 #
-# Well, it means at least n but unlimited (kind of).
+# Well, it means at least n but unlimited max (kind of).
 #
 # Early days, 32_766 was the max limit.
 #
-# But then in Perl v5.30, the limit is now 65_535.
+# But then in Perl v5.30, the limit is 65_535.
 
 use  Test::More;
-
-$_ = 'a' x 32_767;
-ok(/a{1,}/, 'matching upto 32_757');
 
 $_ = 'a' x 65_535;
 ok(/a{1,}/, 'matching upto 65_535');
