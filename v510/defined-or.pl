3dc0103ab60ad6f8d1eb477eca8e53c3cb8a9959
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-05-24 11:49:02 +0100

    - Tidied up defined or.

--- v510/defined-or.pl
+++ v510/defined-or.pl
@@ -3,22 +3,22 @@
 use strict; use warnings;
 use v5.10;
 
-# Early days, logical-or || operator was my friend.
-sub logical_or { my $p = $_[0]; $p ||= 'a'; return $p; }
+# Early days, true-or || operator was my friend.
+sub true_or    { my $p = $_[0]; $p ||= 'a'; return $p; }
 
 # Perl v5.10 introduced defined-or operator //.
 sub defined_or { my $p = $_[0]; $p //= 'a'; return $p; }
 
 use Test::More;
 
-is logical_or(1), '1', 'logical-or (true)  ||=';
+is true_or(1),    '1', 'true-or (true)     ||=';
 is defined_or(1), '1', 'defined-or (true)  //=';
 
-is logical_or( ), 'a', 'logical-or (undef) ||=';
+is true_or( ),    'a', 'true-or (undef)    ||=';
 is defined_or( ), 'a', 'defined-or (undef) //=';
 
 # Below is the difference between the two.
-is logical_or(0), 'a', 'logical-or (false) ||=';
+is true_or(0),    'a', 'true-or (false)    ||=';
 is defined_or(0), '0', 'defined-or (false) //=';
 
 done_testing;
