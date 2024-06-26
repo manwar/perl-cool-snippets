5180fb843c908442882b23105e053c6c23ace3f5
Mohammad Sajid Anwar <Mohammad.Anwar@yahoo.com>
2024-06-26 11:13:15 +0100

    Update class.pl

--- v540/class.pl
+++ v540/class.pl
@@ -3,7 +3,7 @@
 use v5.40;
 use experimental 'class';
 
-class Sum1 {
+class Example1 {
     field $x = __CLASS__->default_x;
     field $y = __CLASS__->default_y;
 
@@ -12,12 +12,12 @@ class Sum1 {
     method sum { return $x + $y }
 }
 
-class Sum2 :isa(Sum1) {
+class Example2 :isa(Example1) {
 
     sub default_x { 1 }
     sub default_y { 2 }
 }
 
-say Sum1->new->sum;
-say Sum2->new->sum;
+say Example1->new->sum;  # 30
+say Example2->new->sum;  # 3
 
